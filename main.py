c0c5075720490f158ec7fee885b8af0fc7566eeb
hemanth <hemanth@hemanths-MacBook-Air.local>
2023-12-17 17:54:42 +0530

    fixed_errors

--- main.py
+++ main.py
@@ -1,37 +1,53 @@
 from datasampler import CIFAR
 from torch.utils.data import TensorDataset, DataLoader
 import torch
+import torch.nn as nn
 import numpy as np
 from model import VIT
+import torch.nn.functional as F
 
-from torch.utils.tensorboard import SummaryWriter
+# from torch.utils.tensorboard import SummaryWriter
 
 def train(dataloader, device, num_epochs):
-    Gen_Losses = []
-    Dis_Losses = []
-    lr = 0.0002
+    Losses = []
+  
+    lr = 0.001
     beta1 = 0.5
     iter = 0
     z_dim, channels_img, f_g = 100, 3, 32
     img_shape = 32,32,3
     f_d = 32
+    patch_dim = 4
+    embed_dim = 32
+    # writer = SummaryWriter(f"logs/loss")
+    algo = VIT(img_shape, patch_dim, embed_dim, img_shape[-1] , 6, 2, 10, 0.1).to(device=device)
+    opt = torch.optim.AdamW(algo.parameters(), lr=lr, betas=[beta1, 0.999])
+    criterion = nn.CrossEntropyLoss()
     
-    writer_real = SummaryWriter(f"logs/real")
-    writer_fake = SummaryWriter(f"logs/fake")
     
-    algo = DCGAN(z_dim,channels_img,f_g,f_d,device,lr,beta1)
     for epoch in range(num_epochs):
-        for i, data in enumerate(dataloader):
-            d_l, g_l = algo.train_step(data[0],iter ,writer_fake,writer_real)
-            Gen_Losses.append(g_l)
-            Dis_Losses.append(d_l)
-            
+        epoch_losses = []
+        tot = 0
+        correct = 0
+        for i, (img, labels) in enumerate(dataloader):
+            opt.zero_grad()
+            output = algo(img)
+            loss = criterion(output, labels)
+            loss.backward()
+            opt.step()
+            epoch_losses.append(loss.item())
+            _, predicted_labels = torch.max(output, 1)
+            _, true_labels = torch.max(labels, 1)
+            tot += len(predicted_labels)
+            correct += (predicted_labels == true_labels).float().sum()
+            accuracy = correct/tot
+             
             if i%50 ==0:
-                print(f'Epoch:{epoch}  {i}/{len(dataloader)}  Gen_loss:{np.array(Gen_Losses).mean()}  Disc_loss:{np.array(Dis_Losses).mean()}')
-                writer_fake.add_scalar('loss',np.array(Gen_Losses).mean(),i)
-                writer_real.add_scalar('loss',np.array(Dis_Losses).mean(),i)
+                print(f'Epoch:{epoch}  {i}/{len(dataloader)}  Loss:{np.array(epoch_losses).mean()} Accuracy:{accuracy}')
+        # writer.add_scalar('loss',np.array(epoch_losses).mean(),epoch)
+       
             
-    return Gen_Losses, Dis_Losses
+    # return Gen_Losses, Dis_Losses
     
 
 def main():
@@ -39,13 +55,13 @@ def main():
     device = torch.device("mps")
     data = CIFAR(path)
     images, labels = data.get_train_data()
-    images_tensor = torch.tensor(images,device=device,dtype=torch.float32)
-    # labels_tensor = torch.tensor(labels,device=device)
+    images_tensor = torch.tensor(images,dtype=torch.float32).to(device)
+    labels_tensor = F.one_hot(torch.tensor(labels).to(torch.int64)).to(torch.float32).to(device)
     
     
-    my_dataset = TensorDataset(images_tensor) 
+    my_dataset = TensorDataset(images_tensor,labels_tensor) 
     my_dataloader = DataLoader(my_dataset,shuffle=True, batch_size=128) 
-    g,d = train(my_dataloader,device,5)
+    train(my_dataloader,device,5)
     
     
 if __name__ == "__main__":
--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from einops.layers.torch import Rearrange
+from einops import repeat
 # party id 57861845
 
 class Attention(nn.Module):
@@ -8,9 +9,9 @@ class Attention(nn.Module):
         super().__init__()
         self.multi_head_attn = torch.nn.MultiheadAttention(embed_dim, num_heads, dropout=0.0)
         
-        self.q = torch.nn.Linear(dim, dim)
-        self.k = torch.nn.Linear(dim, dim)
-        self.v = torch.nn.Linear(dim, dim)
+        self.q = torch.nn.Linear(embed_dim, embed_dim)
+        self.k = torch.nn.Linear(embed_dim, embed_dim)
+        self.v = torch.nn.Linear(embed_dim, embed_dim)
     
     def forward(self, x):
         q = self.q(x)
@@ -20,15 +21,25 @@ class Attention(nn.Module):
         return attn_output
 
 class PreNorm(nn.Module):
-    def __init__(self, fn, embed_dim):
+    def __init__(self, embed_dim, fn):
         super().__init__()
         self.fn = fn 
         self.ln = nn.LayerNorm(embed_dim)
     def forward(self, x, **kwargs):
-        return self.f(self.ln(x), **kwargs)
+        return self.fn(self.ln(x), **kwargs)
+    
+class Residual(nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+    def forward(self, x, **kwargs):
+        res = x
+        x = self.fn(x, **kwargs)
+        x += res
+        return res
         
 class Project_Embedding(nn.Module):
-    def __init__(self, img_size, patch_size, n_c, embed_dim):
+    def __init__(self, patch_size, n_c, embed_dim):
         super().__init__()
         self.rearrange = Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1=patch_size, p2=patch_size)
         self.fc = nn.Linear(n_c * patch_size ** 2, embed_dim)
@@ -42,13 +53,56 @@ class MLP_Head(nn.Sequential):
             nn.Linear(embed_dim, hidden_dim),
             nn.GELU(),
             nn.Dropout(dropout),
-            nn.Linear(hidden_dim, dim),
+            nn.Linear(hidden_dim, embed_dim),
             nn.Dropout(dropout)
         )
         
 
 class VIT(nn.Module):
-    def __init__(self, img_dim, patch_dim, embed_dim, n_channels , n_layers, n_heads, dropout=0.1):
+    def __init__(self, img_dim, patch_dim, embed_dim, n_channels , n_layers, n_heads, out_dim, dropout=0.1):
         super().__init__()
         
-        self.embed_
+        self.height = img_dim[0]
+        self.width = img_dim[1]
+        self.patch_size = patch_dim
+        self.embed_size = embed_dim
+        self.channels = n_channels
+        self.n_layers = n_layers
+        self.heads = n_heads
+   
+        self.patch_embedings = Project_Embedding(patch_dim, n_channels, embed_dim)
+        
+        num_patches = img_dim[0] * img_dim[1]//(patch_dim**2)
+        self.pos_embedding = nn.Parameter(torch.randn(1, num_patches+1, embed_dim))
+        self.cls_token = nn.Parameter(torch.rand(1, 1, embed_dim))
+        
+        self.layers = nn.ModuleList([])
+        
+        for _ in range(n_layers):
+            encoder_block = nn.Sequential(
+                Residual(PreNorm(embed_dim, Attention(embed_dim = embed_dim, num_heads = n_heads, dropout=dropout))),
+                Residual(PreNorm(embed_dim, MLP_Head(embed_dim = embed_dim, hidden_dim = embed_dim, dropout=dropout)))
+            )
+            self.layers.append(encoder_block)
+        self.classification_head = nn.Sequential(
+            nn.LayerNorm(embed_dim),
+            nn.Linear(embed_dim, out_dim)
+        )
+        
+    def forward(self, img):
+        x = self.patch_embedings(img)
+        
+        b, n, _ = x.shape
+        
+        cls_tokens = repeat(self.cls_token, '1 1 d -> b 1 d', b = b)
+        x = torch.cat([cls_tokens, x], dim=1)
+        x += self.pos_embedding[:,:(n+1)]
+        
+        for i in range(self.n_layers):
+            x = self.layers[i](x)
+            
+        x = self.classification_head(x[:, 0, :])
+        
+        return x
+        
+        
